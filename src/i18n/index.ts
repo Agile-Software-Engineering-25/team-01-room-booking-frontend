895f26e0bfbf4645cac71c2e138b5a5987108b3a
0utplay <aldin@sijamhodzic.de>
2025-11-12 20:35:12 +0100

    fix: enable language choosing

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -3,6 +3,8 @@ import { initReactI18next } from 'react-i18next';
 import germanTranslation from './de-DE/translation.json';
 import englishTranslation from './en-US/translation.json';
 
+const savedLang = localStorage.getItem('lang') || 'de';
+
 i18n.use(initReactI18next).init({
   resources: {
     de: {
@@ -12,10 +14,28 @@ i18n.use(initReactI18next).init({
       translation: englishTranslation,
     },
   },
-  lng: 'de',
+  lng: savedLang,
+  fallbackLng: 'de',
   interpolation: {
     escapeValue: false,
   },
 });
+
+window.addEventListener('languageChanged', (event: Event) => {
+  if (event instanceof CustomEvent && event.detail?.lang) {
+    if (event.detail?.lang && event.detail.lang !== i18n.language) {
+      i18n.changeLanguage(event.detail.lang).then();
+    }
+  }
+});
+
+const changeLanguage = (lang: string) => {
+  localStorage.setItem('lang', lang);
+  window.dispatchEvent(
+    new CustomEvent('languageChanged', { detail: { lang } })
+  );
+};
+
+export { changeLanguage };
 
 export default i18n;
